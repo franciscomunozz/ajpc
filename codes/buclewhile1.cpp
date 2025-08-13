bcf136212bc9a96a88422842fd98ad4fab22132a
franciscomunozz <franciscomunozmontecino@gmail.com>
2025-08-13 08:09:42 -0400

    clase 4

---
+++ codes/buclewhile1.cpp
@@ -0,0 +1,3 @@
+while(condición){
+    // Código a repetir
+}
---
+++ codes/ejemplofor.cpp
@@ -0,0 +1,13 @@
+// Programa que imprime todos los números del 1 al N
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; i++){
+        cout << i << endl;
+    }
+    return 0;
+}
